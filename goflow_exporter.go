33d097c815ddcae10ce31bdfcb1cbb47d2fa47bd
Zach Leslie <xaque208@gmail.com>
2020-01-13 19:09:06 -0800

    Functional exporter

--- goflow_exporter.go
+++ goflow_exporter.go
@@ -3,16 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"runtime"
+	"strconv"
 
-	"github.com/cloudflare/goflow/transport"
 	"github.com/cloudflare/goflow/utils"
 	log "github.com/sirupsen/logrus"
+	"github.com/xaque208/goflow_exporter/transport"
 
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	flowmessage "github.com/cloudflare/goflow/pb"
 
 	"sync"
 )
@@ -41,23 +46,89 @@ var (
 	LogLevel = flag.String("loglevel", "info", "Log level")
 	LogFmt   = flag.String("logfmt", "normal", "Log formatter")
 
-	EnableKafka = flag.Bool("kafka", true, "Enable Kafka")
 	MetricsAddr = flag.String("metrics.addr", ":8080", "Metrics address")
 	MetricsPath = flag.String("metrics.path", "/metrics", "Metrics path")
+
+	FlowMetricsPath = flag.String("flowmetrics.path", "/flows", "Flow Metrics path")
 
 	TemplatePath = flag.String("templates.path", "/templates", "NetFlow/IPFIX templates list")
 
 	Version = flag.Bool("v", false, "Print version")
 )
 
+var (
+	flowReceiveBytesTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:      "flow_receive_bytes_total",
+			Help:      "Bytes received.",
+			Subsystem: "flows",
+		},
+		[]string{"source_as", "source_as_name", "destination_as", "destination_as_name", "hostname"},
+	)
+
+	flowTransmitBytesTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:      "flow_transmit_bytes_total",
+			Help:      "Bytes transferred.",
+			Subsystem: "flows",
+		},
+		[]string{"source_as", "source_as_name", "destination_as", "destination_as_name", "hostname"},
+	)
+
+	flowBytes = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:      "flow_bytes",
+			Help:      "Bytes transferred.",
+			Subsystem: "flows",
+		},
+		[]string{
+			"source_address",
+			"destination_address",
+			"source_port",
+			"destination_port",
+			"proto",
+		},
+	)
+)
+
 func init() {
 	transport.RegisterFlags()
+}
+
+func flowHandler(w http.ResponseWriter, r *http.Request) {
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(flowReceiveBytesTotal)
+	registry.MustRegister(flowTransmitBytesTotal)
+	registry.MustRegister(flowBytes)
+
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
 }
 
 func httpServer(state *utils.StateNetFlow) {
 	http.Handle(*MetricsPath, promhttp.Handler())
 	http.HandleFunc(*TemplatePath, state.ServeHTTPTemplates)
+
+	http.HandleFunc(*FlowMetricsPath, flowHandler)
+
 	log.Fatal(http.ListenAndServe(*MetricsAddr, nil))
+}
+
+func flowReceiver(flowChan chan *flowmessage.FlowMessage) {
+	for f := range flowChan {
+
+		flowBytes.With(
+			prometheus.Labels{
+				"source_address":      net.IP(f.SrcAddr).String(),
+				"destination_address": net.IP(f.DstAddr).String(),
+				"source_port":         strconv.Itoa(int(f.SrcPort)),
+				"destination_port":    strconv.Itoa(int(f.DstPort)),
+				"proto":               strconv.Itoa(int(f.Proto)),
+			},
+		).Add(float64(f.Bytes))
+
+	}
+
 }
 
 func main() {
@@ -99,15 +170,17 @@ func main() {
 
 	go httpServer(sNF)
 
-	if *EnableKafka {
-		kafkaState, err := transport.StartKafkaProducerFromArgs(log.StandardLogger())
-		if err != nil {
-			log.Fatal(err)
-		}
-		sSFlow.Transport = kafkaState
-		sNFL.Transport = kafkaState
-		sNF.Transport = kafkaState
+	flowChan := make(chan *flowmessage.FlowMessage)
+	go flowReceiver(flowChan)
+
+	promState, err := transport.StartPromProducerFromArgs(flowChan, log.StandardLogger())
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	sSFlow.Transport = promState
+	sNFL.Transport = promState
+	sNF.Transport = promState
 
 	wg := &sync.WaitGroup{}
 	if *SFlowEnable {
